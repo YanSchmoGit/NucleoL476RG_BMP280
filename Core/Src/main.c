381beab8a36ab78dd8cd18eba5dac548c56f5ee4
Yannick <test@test>
2026-04-04 21:25:22 +0200

    USART2 to Terminal added

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,4 +1,3 @@
-
 /*
  * Test project for I2C functions
  * Used Hardware:
@@ -9,11 +8,15 @@
 
 #include <stm32l4xx.h>
 #include <system_stm32l4xx.h>
+#include <stdio.h>
 
 
+
+#include "Init.h"
 #include "LCDFunctions.h"
 #include "I2C.h"
 #include "BMP280.h"
+#include "USART_Terminal.h"
 
 #define I2C_SCL_Pin 8
 #define I2C_SCL_Port GPIOB
@@ -23,45 +26,61 @@
 
 #define BMP280_DEVICE_ADR  (0x77)
 
+#define BAUDRATE (uint8_t) (115200)
+
 uint32_t valuePress;
 int32_t valueTemp;
 
-uint8_t testData[5];
+
 
 
 int main(void)
 {
+    // Init functions;
+    // Init SysClock
 
-  LCDSetup();
-
- InitI2C(I2C_SCL_Port, I2C_SCL_Pin, I2C_SDA_Port, I2C_SDA_Pin);
-
-  InitBMP280(BMP280_DEVICE_ADR);
-
-  GetSensorCalibrationData(BMP280_DEVICE_ADR);
+    InitClock();
 
 
+    // Init I2C
 
-  while (1)
-  {
+    InitI2C(I2C_SCL_Port, I2C_SCL_Pin, I2C_SDA_Port, I2C_SDA_Pin);
 
-    GetSensorValues(BMP280_DEVICE_ADR, &valuePress, &valueTemp);
+    // Init BMP280;
+    InitBMP280(BMP280_DEVICE_ADR);
+    GetSensorCalibrationData(BMP280_DEVICE_ADR);
 
+    // Init USART
 
-    LCDSetCursorLocation(0, 0);
-    LCDSendInteger(valueTemp / 100);
-    LCDSendString(".");
-    LCDSendInteger((valueTemp / 10) % 10);
-    LCDSendString(" C");
-    LCDSetCursorLocation(0, 1);
+    InitUSART(BAUDRATE);
 
-    LCDSendInteger(valuePress/ 100);
-    LCDSendString(".");
-    LCDSendInteger((valuePress / 10) % 10);
-    LCDSendString(" hPa");
+    //SendAString("Hallo");
+    //SendAString("Hier koennte Ihre Werbung stehen!");
+    //GetSensorValues(BMP280_DEVICE_ADR, &valuePress, &valueTemp);
+    //int32_t test = 11111;
 
+    //SendAInteger(valueTemp);
 
+    while (1)
+    {
+        GetSensorValues(BMP280_DEVICE_ADR, &valuePress, &valueTemp);
 
-  }
+/*
+        LCDSetCursorLocation(0, 0);
+        LCDSendInteger(valueTemp / 100);
+        LCDSendString(".");
+        LCDSendInteger((valueTemp / 10) % 10);
+        LCDSendString(" C");
+        LCDSetCursorLocation(0, 1);
 
+        LCDSendInteger(valuePress / 100);
+        LCDSendString(".");
+        LCDSendInteger((valuePress / 10) % 10);
+        LCDSendString(" hPa");
+
+*/
+        SendAInteger(valueTemp);
+    }
 }
+
+
